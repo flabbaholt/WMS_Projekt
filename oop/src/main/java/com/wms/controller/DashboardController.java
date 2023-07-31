1eb924f27efb49a1283b71b82c4df0b4c10e056b
flabbaholt <louis.lsv7@gmail.com>
2023-07-31 21:02:17 +0200

    added imports to dashboardcontroller

--- oop/src/main/java/com/wms/controller/DashboardController.java
+++ oop/src/main/java/com/wms/controller/DashboardController.java
@@ -8,6 +8,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.util.Callback;
 
 public class DashboardController {
  
