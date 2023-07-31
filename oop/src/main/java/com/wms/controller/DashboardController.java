c4c4ed328885a691788b6ccca02dc14db7ec741b
Ajax1907 <konstantin.berecz@gmail.com>
2023-07-31 20:25:24 +0200

    Dashboard

--- oop/src/main/java/com/wms/controller/DashboardController.java
+++ oop/src/main/java/com/wms/controller/DashboardController.java
@@ -10,5 +10,42 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class DashboardController {
-    
+ 
+    public void initialize() {
+        // Erstelle die Zelle, um Buttons im TableView anzuzeigen
+        buttonColumn.setCellFactory(new Callback<TableColumn<String, String>, TableCell<String, String>>() {
+            @Override
+            public TableCell<String, String> call(TableColumn<String, String> param) {
+                return new TableCell<String, String>() {
+                    final Button button = new Button();
+
+                    {
+                        button.setOnAction(event -> {
+                            String rowData = getTableView().getItems().get(getIndex());
+                            System.out.println("Button clicked for row: " + rowData);
+                        });
+                    }
+
+                    @Override
+                    protected void updateItem(String item, boolean empty) {
+                        super.updateItem(item, empty);
+
+                        if (item == null || empty) {
+                            setGraphic(null);
+                        } else {
+                            button.setText(item);
+                            setGraphic(button);
+                        }
+                    }
+                };
+            }
+        });
+
+        // Fülle den TableView mit den Button-Daten
+        ObservableList<String> data = FXCollections.observableArrayList();
+        for (int i = 0; i < 10; i++) {
+            data.add("Button " + (i + 1));
+        }
+        tableView.setItems(data);
+    }
 }
