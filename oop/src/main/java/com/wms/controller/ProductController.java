3d14cc04e3148f93262a66d204903d73324bd0ce
Nils <70151748+nilsulrichpdm@users.noreply.github.com>
2023-08-03 09:14:36 +0200

    Update ProductController.java
    Beschreibung der "Add-Button gedrückt" Methode

--- oop/src/main/java/com/wms/controller/ProductController.java
+++ oop/src/main/java/com/wms/controller/ProductController.java
@@ -1,5 +1,15 @@
 package com.wms.controller;
 
 public class ProductController {
+
+
+    /*Add-Button wurde gedrückt DAS IST NUR PSEUDOCODE
+    *String[] attributes = get Data();
+    *Product a = new Product(attributes);
+    *try {
+        a.saveData()
+
+    *}catch (DB.exception);
+    */
     
 }
