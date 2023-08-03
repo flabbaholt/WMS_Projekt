48bf7f2dd16e79c022507f84bc4e9a25277539a0
flabbaholt <louis.lsv7@gmail.com>
2023-08-03 21:47:37 +0200

    New Product.java
    added old in temporary backup folder

--- oop/src/main/java/com/wms/tempbackup/ProductOutdated.java
+++ oop/src/main/java/com/wms/tempbackup/ProductOutdated.java
@@ -2,70 +2,69 @@ package com.wms.model;
 
 import com.wms.model.interfaces.Displayable;
 
-public class Product implements Displayable {
+
+//ACHTUNG: WIRD NICHT MEHR BENUTZT - ProductSearchModel wird benutzt
+public class ProductOutdated implements Displayable{
+    // attributes
+    private int id;
+    private int stellplatz_id;
+    private String productName;
+    private String manufacturer;
+    private String arrivalTime;
+
+    //Produktkurzberschreibung 
     
-    Integer productID;
-    String productNumber, stellplatzID, productName, manufacturer, arrivalTime;
-
-
-    public Product(Integer productID, String productNumber, String stellplatzID, String productName, String manufacturer, String arrivalTime) {
-        this.productID = productID;
-        this.productNumber = productNumber;
-        this.stellplatzID = stellplatzID;
+    // constructer
+    public ProductOutdated(int id, int stellplatz_id, String productName, String manufacturer, String arrivalTime) {
+        this.id = id;
+        this.stellplatz_id = stellplatz_id;
         this.productName = productName;
         this.manufacturer = manufacturer;
         this.arrivalTime = arrivalTime;
     }
 
-
-    public Integer getProductID() {
-        return this.productID;
+    // getter - methods
+    public int getId() {
+        return this.id;
     }
 
-    public void setProductID(Integer productID) {
-        this.productID = productID;
+    public int getStellplatz_id() {
+        return this.stellplatz_id;
     }
-
-    public String getProductNumber() {
-        return this.productNumber;
-    }
-
-    public void setProductNumber(String productNumber) {
-        this.productNumber = productNumber;
-    }
-
-    public String getStellplatzID() {
-        return this.stellplatzID;
-    }
-
-    public void setStellplatzID(String stellplatzID) {
-        this.stellplatzID = stellplatzID;
-    }
-
+    
     public String getProductName() {
         return this.productName;
+    }
+
+    public String getManufacturer() {
+        return this.manufacturer;
+    }
+    
+    public String getArrivalTime() {
+        return this.arrivalTime;
+    }
+
+    // setter - methods
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setStellplatz_id(int stellplatz_id) {
+        this.stellplatz_id = stellplatz_id;
     }
 
     public void setProductName(String productName) {
         this.productName = productName;
     }
 
-    public String getManufacturer() {
-        return this.manufacturer;
-    }
-
     public void setManufacturer(String manufacturer) {
         this.manufacturer = manufacturer;
-    }
-
-    public String getArrivalTime() {
-        return this.arrivalTime;
     }
 
     public void setArrivalTime(String arrivalTime) {
         this.arrivalTime = arrivalTime;
     }
-
+    
     // todo
     private boolean checkInput() {
         //code
@@ -86,8 +85,8 @@ public class Product implements Displayable {
         return true;
     }
 
-
     
+     
     @Override
     public String[] getAttributes() {
         // TODO Auto-generated method stub
