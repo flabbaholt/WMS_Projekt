38fb67c54031238efed34440e499992a8056dc76
flabbaholt <louis.lsv7@gmail.com>
2023-08-03 21:49:49 +0200

    Update App.java

--- oop/src/main/java/com/wms/App.java
+++ oop/src/main/java/com/wms/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("views/Dashboard_Scene"), 640, 480);
+        scene = new Scene(loadFXML("views/Dashboard_Scene"), 1080, 500);
         stage.setScene(scene);
         stage.show();
     }
