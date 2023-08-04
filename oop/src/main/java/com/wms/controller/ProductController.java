9cbb77b8b25a5870014401152be762ac4ddbce17
Nils <70151748+nilsulrichpdm@users.noreply.github.com>
2023-08-04 16:25:57 +0200

    ProductController erstellt

--- oop/src/main/java/com/wms/controller/ProductController.java
+++ oop/src/main/java/com/wms/controller/ProductController.java
@@ -5,17 +5,50 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class ProductController implements Initializable {
-    
+
     @FXML
-    public void saveNewProduct(){
+    private TextField amountField;
+    @FXML
+    private TextField stellplatzIDField;
+    @FXML
+    private TextField productNumberField;
+    @FXML
+    private TextField productNameField;
+    @FXML
+    private TextField manufacturerField;
+    @FXML
+    private Button submitAdd;
+    @FXML
+    private Button cancelAdd;
+
+    //Es fehlt noch die Unterscheidung zu !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+    @FXML 
+    public void handleSubmitButton(MouseEvent event){
+        
+
+        
+
+    }
+
+    private void closeWindow() {
+        // Close the window (Stage) when the cancel button is clicked
+        cancelAdd.getScene().getWindow().hide();
+    }
+
+    private void saveProduct(){
 
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+        // Initialize the controller
+        submitAdd.setOnAction(event -> saveProduct());
+        cancelAdd.setOnAction(event -> closeWindow());        
     }
 }
